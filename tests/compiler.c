9050c0d1cbc2705ecf17465e36e96737ddf22abf
Pedro Gonzalez <pgg1998@gmail.com>
2024-02-04 21:15:40 -0800

    Add test helper

--- tests/compiler.c
+++ tests/compiler.c
@@ -5,13 +5,18 @@
 #include "../src/token.c"
 #include "test.h"
 
-void test_num(void) {
-  Parser parser = new_parser("10");
+void run_compiler_test(const char* input, const char* expected) {
+  Parser parser = new_parser(input);
   Token* tkn = parse(&parser);
   Compiler cs = new_compiler(tkn);
   compile(&cs);
-  bool res = array_compare_with_string(cs.code, "const 10 0");
+  bool res = array_compare_with_string(cs.code, expected);
   TEST_EQ(res, true);
+}
+
+void test_num(void) {
+  run_compiler_test("10", "const 10 0");
+  run_compiler_test("1000", "const 1000 0");
 }
 
 int main(void) {
